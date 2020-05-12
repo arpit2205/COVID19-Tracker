6e0c8c0f3bb7828395376d1043bc1f8dff63c901
arpit2205 <arpit.8.a@gmail.com>
2020-05-12 14:28:38 +0530

    Initial Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <h5 className={styles.text}>COVID-19 TRACKER APP</h5>
+        </div>
+    )
+}
+
+export default Footer
